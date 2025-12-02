7eb6fc22fb5b907943c1e5dee7ec2c46edf01a76
Andrii <aksaninak@gmail.com>
2025-12-02 11:24:43 +0200

    Prototype prepearing

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,13 @@
+using System.Diagnostics;
 using UnityEngine;
 
+[DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
 public class GameManager
 {
-    
+
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
 }
